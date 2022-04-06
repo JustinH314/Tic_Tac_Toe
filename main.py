bb373927f4daf7bccd370aa15f5d7984725264d4
Justin <101justinbii@gmail.com>
2022-04-06 20:36:16 +0800

    add draw sign

--- main.py
+++ main.py
@@ -8,11 +8,39 @@ SCREEN_SIZE = (HEIGHT, WIDTH)
 GAME_TITLE = "Tic Tac Toe"
 STROKE_WIDTH = 5
 STROKE_COLOR = (255, 255, 255)
+SIGN_COLOR = (246, 174, 45)
+CROSS_MARGIN = 40
 def draw_line():
     pygame.draw.line(screen, STROKE_COLOR, (0, HEIGHT/3), (WIDTH, HEIGHT/3), STROKE_WIDTH)
     pygame.draw.line(screen, STROKE_COLOR, (0, HEIGHT/3*2), (WIDTH, HEIGHT/3*2), STROKE_WIDTH)
     pygame.draw.line(screen, STROKE_COLOR, (WIDTH/3, 0), (WIDTH/3, HEIGHT), STROKE_WIDTH)
     pygame.draw.line(screen, STROKE_COLOR, (WIDTH/3*2, 0), (WIDTH/3*2, HEIGHT), STROKE_WIDTH)
+
+def draw_circle(row, col):
+    pygame.draw.circle(screen, SIGN_COLOR, (int(col * WIDTH/3 + WIDTH/6), int(row * HEIGHT/3 + HEIGHT/6)), HEIGHT/6 * 0.7, STROKE_WIDTH)
+
+    # [[1, 2, 3],
+    #  [4, 5, 6],
+    #  [7, 8, 9]]
+
+def draw_cross(row, col):
+    pygame.draw.line(screen,
+                     SIGN_COLOR,
+                     (int(col * WIDTH/3 + CROSS_MARGIN), int(row * HEIGHT/3 + CROSS_MARGIN)),
+                     (int(col * WIDTH/3 + WIDTH/3 - CROSS_MARGIN), int(row * HEIGHT/3 + HEIGHT/3 - CROSS_MARGIN)), 
+                     STROKE_WIDTH)
+    pygame.draw.line(screen,
+                     SIGN_COLOR,
+                     (int(col * WIDTH/3 + CROSS_MARGIN), int(row * HEIGHT/3 + HEIGHT/3 - CROSS_MARGIN)),
+                     (int(col * WIDTH/3 + WIDTH/3 - CROSS_MARGIN), int(row * HEIGHT/3 + CROSS_MARGIN)), 
+                     STROKE_WIDTH)
+#:%s/pattern/replace/g 
+
+def draw_sign(player, row, col):
+    if player == 0:
+        draw_circle(row, col)
+    elif player == 1:
+        draw_cross(row, col)
 
 if "__main__" == __name__:
     pygame.init()
@@ -29,6 +57,6 @@ if "__main__" == __name__:
                 pos_x, pos_y = event.pos
                 col = int(pos_x // (WIDTH/3))
                 row = int(pos_y // (HEIGHT/3))
-                print(col)
-        
+                print(row, col)
+                draw_cross(row, col)
         pygame.display.update()            
