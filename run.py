9ca59ef49b9a721570544ed064e8b0eddf488dca
Justin <101justinbii@gmail.com>
2022-04-20 20:38:55 +0800

    add dynamic crosshair

--- run.py
+++ run.py
@@ -7,7 +7,7 @@ import sys
 # game settings
 ASSETS_DIR = "assets"
 BACKGROUND_IMAGE = "background.jpg"
-CROSSHAIR_IMAGE = "crosshair.png"
+CROSSHAIR_IMAGE = "crosshair 2.png"
 SCREEN_WIDTH = 1920
 SCREEN_HEIGHT = 1080
 SCREEN_SIZE = (SCREEN_WIDTH, SCREEN_HEIGHT)
@@ -15,8 +15,12 @@ SCREEN_SIZE = (SCREEN_WIDTH, SCREEN_HEIGHT)
 class Crosshair(pygame.sprite.Sprite):
     def __init__(self, filename):
         super().__init__()
-        self.image = pygame.image.load(ASSETS_DIR + CROSSHAIR_IMAGE)
+        self.image = pygame.image.load(Path(ASSETS_DIR) / filename)
+        self.image = pygame.transform.scale(self.image, (80, 80))
+        self.rect = self.image.get_rect()
 
+    def update(self):
+        self.rect.center = pygame.mouse.get_pos()
 
 
 if "__main__" == __name__:
@@ -24,10 +28,14 @@ if "__main__" == __name__:
 
     # initialization
     pygame.init()
+    pygame.mouse.set_visible(False)
     screen = pygame.display.set_mode(SCREEN_SIZE)
     pygame.display.set_caption("Tic Tac Toe")
 
     background = pygame.image.load(Path(ASSETS_DIR) / BACKGROUND_IMAGE)
+    group = pygame.sprite.Group()
+    crosshair = Crosshair(CROSSHAIR_IMAGE)
+    group.add(crosshair)
 
     while running:
         for event in pygame.event.get():
@@ -37,4 +45,5 @@ if "__main__" == __name__:
         
         pygame.display.flip()
         screen.blit(background, (0, 0))
-            
+        group.draw(screen)
+        group.update()
